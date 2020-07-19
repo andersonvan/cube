c1f9dcd9b1302ed2363be31a713c4e68c8fcb6e9
anderson <597909750@qq.com>
2020-07-19 22:47:05 +0800

    cube样式构建ing

---
+++ src/components/Cube.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="my-triangle">2</div>
+    <div class="my-triangle">2</div>
+    <div class="my-triangle">2</div>
+    <div class="my-triangle">2</div>
+    <div class="my-triangle">2</div>
+    <div class="my-triangle">2</div>
+    <div class="my-triangle">2</div>
+    <div class="my-triangle">2</div>
+    <div class="my-triangle">2</div>
+    <div id="tt"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Cube',
+  components: {},
+  props: {
+    setHight: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {
+//     triangle(@height){
+//   @h: @height;
+//   @w: tan(30deg) * 2 * @h;
+//   width: @w;
+//   height: @h;
+//   clip-path: polygon(@w/2 0, @w @h, 0 @h);
+// }
+  }
+}
+
+</script>
+<style scoped lang="less">
+@height: 100px;
+.equ-triangle{
+  @h: @height;
+  // @w: tan(30deg) * 2 * @h;
+  @w: tan(30deg) * 2 * @h;
+  width: @w;
+  height: @h;
+  clip-path: polygon(@w/2 0, @w @h, 0 @h);
+}
+.my-triangle {
+  .equ-triangle;
+  background: #f00;
+}
+#tt {
+  width: 100px;
+  height: 100px;
+  background-color: black;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,128 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <div id="cube">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+      <div class="fillter"></div>
+      <div class="fillter"></div>
+      <div class="fillter"></div>
+      <div class="fillter"></div>
+      <div class="fillter"></div>
+      <div class="fillter"></div>
+      <div class="fillter"></div>
+      <div class="fillter"></div>
+      <div class="fillter"></div>
+    </div>
+    <div id="test"></div>
+    <cube :setHight="hight"/>
   </div>
 </template>
-
 <script>
+import Cube from './Cube'
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: '',
+  components: {Cube},
+  props: {},
+  data () {
+    return {
+      hight:'500px'
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {
+    var oDiv = document.querySelector('#cube');
+    var x = 30;
+    var y = -60;
+    oDiv.onmousedown = function(ev){
+       var event = window.event || ev;
+       var disY = event.clientX - y;
+       var disX = event.clientY - x;
+       document.onmousemove = function(ev){
+           var event = window.event || ev;
+           // 计算偏移角度
+           x = event.clientY - disX;
+           y = event.clientX - disY;
+           oDiv.style.transform = 'perspective(800px) rotateY('+y+'deg) rotateX('+x+'deg)'
+       }
+       document.onmouseup = function(){
+           document.onmousemove = null;
+       }
+       return false;
+   }
+  },
+  methods: {}
 }
 </script>
+<style scoped lang="less">
+#cube {
+  position:relative;
+  top:300px;
+  left:0;
+  width: 200px;
+  height:200px;
+  // margin: 300px auto;
+  // border:1px solid #333;
+  margin:0px auto;
+  transform-style: preserve-3d;
+  transform: rotate3d(1, 1, 0, -45deg);
+}
+#cube > div {
+  // width: 50px;
+  // height: 50px;
+  height: 100%;
+  width: 100%;
+  border: 1px solid #eee;
+  position: absolute;
+  top: 0;
+  left: 0;
+  text-align: center;
+  line-height: 200px;
+  clip-path: polygon(25% 0%, 75% 0%, 100% 25%, 100% 75%, 75% 100%, 25% 100%, 0% 75%, 0% 25%)
+}
+#cube > div:nth-child(1){
+  transform:translateZ(100px);
+  background-color: red;
+}
+#cube > div:nth-child(2){
+  transform:translateZ(-100px);
+  background-color: rgb(0, 38, 255);
+}
+#cube > div:nth-child(3){
+  transform:rotateY(90deg) translateZ(100px);
+  background-color: rgb(0, 255, 0);
+}
+#cube > div:nth-child(4){
+  transform:rotateY(-90deg) translateZ(100px);
+  background-color: rgb(251, 255, 0);
+}
+#cube > div:nth-child(5){
+  transform:rotateX(90deg) translateZ(100px);
+  background-color: rgb(253, 253, 253);
+}
+#cube > div:nth-child(6){
+  transform:rotateX(-90deg) translateZ(100px);
+  background-color: rgb(0, 0, 0);
+}
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+#cube > div.fillter{
+  background-color: orange;
+  clip-path: polygon(50% 0, 50% 50%,0% 50%)
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+#test {
+  position: absolute;
+  top: 100px;
+  left: 300px;
+  height: 200px;
+  width: 200px;
+  background-color: lightgreen;
+  clip-path: polygon(22% 0, 44% 22%,0% 22%)
+
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
